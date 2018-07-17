838f0cc848a3d99b1ac84cbd85705fbf5838c8b1
Ignacio <royovillanova.i@gmail.com>
2018-07-17 19:38:54 +0200

    Dev (#12)
    * github data
    
    * movido store de usuario a módulo
    
    * adaptado componente login
    
    * eliminada redirección al hacer log out
    
    * obteniendo datos de usuario de la store
    
    * correccion cierre modal al iniciar app
    
    * refactor obtener usuario
    
    * eliminadas llamadas innecesarias a firebase

--- client/src/Services/auth.service.js
+++ client/src/Services/auth.service.js
@@ -1,56 +1,63 @@
 import firebase from 'firebase'
-import Router from '../router/index.js'
+import axios from 'axios'
 
 const provider = new firebase.auth.GithubAuthProvider()
 export default {
   getCurrentUser () {
     return new Promise(async (resolve, reject) => {
       try {
-        var firebaseUser = await firebase.auth().currentUser
-        if (firebaseUser) {
-          let doc = await firebase.firestore().collection('User').doc(firebaseUser.uid).get()
-          if (doc.exists) {
-            var currentUser = doc.data()
-            resolve(currentUser.userData)
-          } else {
-            console.log('no existe el usuario solicitado')
-            resolve(undefined)
-          }
-        }
+        let firebaseUser = await firebase.auth().currentUser
+        if (!firebaseUser) resolve(null)
+
+        let doc = await firebase.firestore().collection('User').doc(firebaseUser.uid).get()
+        if (!doc.exists) reject(new Error('user doesn`t exist'))
+
+        let currentUser = doc.data()
+        resolve(currentUser.userData)
       } catch (error) {
         reject(new Error(`Unable to get current user! ${error}`))
       }
     })
   },
-
   login () {
-    return new Promise((resolve, reject) => {
-      firebase.auth().signInWithPopup(provider).then((result) => {
-        // retrieve the user info and store it in firebase db for custom properties of the user
-        var userData = JSON.stringify(result.user)
+    return new Promise(async (resolve, reject) => {
+      try {
+        // User firebase github login provider
+        let auth = await firebase.auth().signInWithPopup(provider)
+        // Get github's user accessToken
+        const token = auth.credential.accessToken
+        // Use github API and fetch user info
+        let gitHubData = await this.getGitHubData(token)
+        let userData = JSON.stringify(auth.user)
         userData = JSON.parse(userData)
+        userData.gitHubData = gitHubData
         // Store the user in /user/{{uid}}/datos...
-        firebase.firestore().collection('User').doc(userData.uid).set({userData})
-          .then((resp) => {
-            resolve(userData)
-          })
-      }).catch((error) => {
-        console.log('Unable to Log in!' + error)
+        await firebase.firestore().collection('User').doc(userData.uid).set({userData})
+        resolve(userData)
+      } catch (error) {
         reject(new Error(`Unable to Log in! ${error}`))
-      })
+      }
     })
   },
   // Log out
   logout () {
     return new Promise((resolve, reject) => {
       firebase.auth().signOut().then(() => {
-        console.log('Logout success!')
         resolve(true)
-        // Redirect to home
-        Router.push('/')
       }).catch((error) => {
         reject(error)
       })
+    })
+  },
+
+  async getGitHubData (token) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let data = await axios.get('https://api.github.com/user?access_token=' + token)
+        resolve(data.data)
+      } catch (error) {
+        reject(error)
+      }
     })
   }
 }
